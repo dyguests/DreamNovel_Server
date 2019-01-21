5c7128545e43cc70db7bddd095a4448a1ec2771e
fanhl <dyguests@qq.com>
2019-01-21 22:48:59 +0800

    kotlin

---
+++ src/main/java/com/fanhl/dreamnovel/controller/MainController.kt
@@ -0,0 +1,26 @@
+package com.fanhl.dreamnovel.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+/**
+ * 主控件器
+ *
+ * @author fanhl
+ */
+@Controller
+class MainController {
+    /**
+     * 首页
+     *
+     * @return 首页
+     */
+    @RequestMapping(value = ["/"], method = arrayOf(RequestMethod.GET))
+    fun index(modelMap: ModelMap): String {
+        modelMap["Title"] = "你好 SpringMVC3"
+        modelMap["END"] = "你好 SpringMVC3!"
+        return "index"
+    }
+}
