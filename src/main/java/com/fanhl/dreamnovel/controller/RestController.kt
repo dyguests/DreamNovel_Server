a623335c3e004bde9cfb6e505525a0e68e9182f9
fanhl <dyguests@qq.com>
2019-01-22 20:51:08 +0800

    RestController

---
+++ src/main/java/com/fanhl/dreamnovel/controller/RestController.kt
@@ -0,0 +1,20 @@
+package com.fanhl.dreamnovel.controller
+
+import com.fanhl.dreamnovel.model.Greeting
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+
+@RestController
+class RestController {
+    private val template = "Hello, %s!"
+    private val counter = AtomicLong()
+
+    @RequestMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
+        return Greeting(counter.incrementAndGet(),
+                String.format(template, name))
+    }
+}
---
+++ src/main/java/com/fanhl/dreamnovel/model/Greeting.kt
@@ -0,0 +1,3 @@
+package com.fanhl.dreamnovel.model
+
+class Greeting(val id: Long, val content: String)
